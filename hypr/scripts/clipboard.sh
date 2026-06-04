58af07c7b0d5d35b477b04fbbc59c4fd1f39e760
brianrscode <135724962+brianrscode@users.noreply.github.com>
2026-06-03 23:36:08 -0600

    Modifico clipboard para agregar boton de borrar todo y modifico su tamaño

--- hypr/scripts/clipboard.sh
+++ hypr/scripts/clipboard.sh
@@ -1,6 +1,34 @@
 #!/bin/bash
 
-cliphist list |
-    rofi -theme ~/.config/hypr/rofi/clipboard/clipboard.rasi -dmenu -sync -display-columns 2 |
-    cliphist decode |
-    wl-copy
+THEME="$HOME/.config/hypr/rofi/clipboard/clipboard.rasi"
+CLEAR_OPTION="󰆴  Borrar todo"
+
+selection="$(
+    {
+        echo "$CLEAR_OPTION"
+        cliphist list
+    } | rofi \
+        -theme "$THEME" \
+        -dmenu \
+        -sync \
+        -i \
+        -p ""
+)"
+
+case "$selection" in
+"$CLEAR_OPTION")
+    cliphist wipe
+    notify-send "Portapapeles" "Historial borrado" -i user-trash -t 1500
+    exit 0
+    ;;
+
+"")
+    exit 0
+    ;;
+
+*)
+    echo "$selection" | cliphist decode | wl-copy
+    notify-send "Portapapeles" "Copiado al portapapeles"
+    exit 0
+    ;;
+esac
